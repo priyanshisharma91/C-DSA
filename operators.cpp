31fd00eb42224e6ddc62cdbc9d68521375a81bea
priyanshisharma91 <priyanshisharma1234ps@gmail.com>
2026-09-22 22:09:24 +0530

    Initial commit: setting up C++ DSA repository

---
+++ operators.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float a,b;
+    cout<<"Enter two numbers: ";
+    cin>>a>>b;
+    cout<<"division "<<a<<" and "<<b<<" is: "<<a/b<<endl;
+    return 0;
+}
